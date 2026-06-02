8a05b38e543c8f73554214c82256d54a9b81fe71
AcademyAspNet <208851284+AcademyAspNet@users.noreply.github.com>
2026-06-02 22:01:06 +0300

    Initial commit

---
+++ projects/java-messenger/src/ru/academy/messenger/tcp/TcpClient.java
@@ -0,0 +1,33 @@
+package ru.academy.messenger.tcp;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TcpClient {
+
+    static void main() throws IOException, InterruptedException {
+        System.out.println("Подключение к серверу...");
+
+        try (Socket socket = new Socket("127.0.0.1", 27015)) {
+
+            System.out.println("Соединение успешно установлено!");
+
+            PrintWriter writer = new PrintWriter(
+                    socket.getOutputStream(),
+                    true
+            );
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            while (!socket.isClosed()) {
+                String message = IO.readln("Введите Ваше сообщение: ");
+                writer.println(message);
+
+                String receivedMessage = reader.readLine();
+                System.out.println("Сообщение от сервера: " + receivedMessage);
+            }
+        }
+    }
+}
---
+++ projects/java-messenger/src/ru/academy/messenger/tcp/TcpServer.java
@@ -0,0 +1,48 @@
+package ru.academy.messenger.tcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServer {
+
+    static void main() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(27015)) {
+
+            System.out.println("Серверный сокет инициализирован");
+            Socket clientSocket = serverSocket.accept();
+
+            System.out.println("Клиент подключился к серверу!");
+
+            PrintWriter writer = new PrintWriter(
+                    clientSocket.getOutputStream(),
+                    true
+            );
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream())
+            );
+
+            while (!clientSocket.isClosed()) {
+                String receivedMessage = reader.readLine();
+
+                System.out.println(
+                        "От клиента получено сообщение: " + receivedMessage
+                );
+
+                String reversedMessage = new StringBuilder(receivedMessage)
+                        .reverse()
+                        .toString();
+
+                writer.println(reversedMessage);
+
+                System.out.println(
+                        "Перевернули символы в строке: " + reversedMessage
+                );
+            }
+        }
+    }
+}
